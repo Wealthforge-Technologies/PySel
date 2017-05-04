22f76c48625ffd82af39b7b04624b7200e73b721
David Koslow <dkoslow@wealthforge.com>
2017-05-04 11:14:19 -0400

    rename for naming convention

--- testsuites/bd_login_testsuite.py
+++ testsuites/bd_login_testsuite.py
@@ -1,14 +1,14 @@
 import unittest
 import sys
 import os
-from testcases import Login
+from testcases import bd_login_testcase
 
 
 def main():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    suite.addTests(loader.loadTestsFromModule(Login))
+    suite.addTests(loader.loadTestsFromModule(bd_login_testcase))
 
     runner = unittest.TextTestRunner(verbosity=3)
     result = runner.run(suite)
