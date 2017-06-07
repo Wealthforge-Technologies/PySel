bfa15ed4f0c37bfe188e1ebc1855e6a6e72d376c
David Koslow <dkoslow@wealthforge.com>
2017-06-07 11:06:03 -0400

    no message

---
+++ testsuites/testcases/testpages/bdbanktab_page.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from .element import PageElement
+from .testpageutilities.waitforangular import waitForAngular
+from .basepage import BasePage
+
+class BDBankTabPage(BasePage):
+    # https://qa1.wealthforge.org/BD/#/banks
+    create_bank_btn = PageElement(xpath="//nav[contains(@label,'Create Bank')]")
+
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.expected_landing_url = "https://qa1.wealthforge.org/BD/#/banks"
+        self.expected_title = "WF: Broker Dealer"
+
+    def is_expected_title(self):
+        try:
+            wait = WebDriverWait(self.driver, 5).until(
+                EC.title_contains(self.expected_title))
+        finally:
+            assert self.expected_title in self.driver.title
+        waitForAngular(self.driver)
+
+
+    def is_expected_landing_url(self):
+        try:
+            wait = WebDriverWait(self.driver, 5).until(
+                lambda wait: self.driver.current_url == self.expected_landing_url)
+        finally:
+            assert self.expected_landing_url in self.driver.current_url
+        waitForAngular(self.driver)
+
+
+    def land(self):
+        self.driver.get(self.expected_landing_url)
+        waitForAngular(self.driver)
